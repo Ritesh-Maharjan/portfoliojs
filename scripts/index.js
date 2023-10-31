815175a12453009f004c36e31b1106a180d9f5b7
Ritesh <riteshmaharjan07@gmail.com>
2023-10-30 22:03:32 -0700

    Final Edit

--- scripts/index.js
+++ scripts/index.js
@@ -18,8 +18,9 @@ hamburgerMenu.addEventListener("click", () => {
 
 changeColor.addEventListener("click", () => {
   if (nightMode) {
-    rootEl.style.setProperty("--primary-color", "#f5f5f5");
     rootEl.style.setProperty("--secondary-color", "#1d1d1d");
+    rootEl.style.setProperty("--primary-color", "#f5f5f5");
+    rootEl.style.setProperty("--tech-color", "#0055ff");
     nightMode = !nightMode;
     changeColor.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16"> <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/> </svg>
     Dark`;
@@ -27,6 +28,7 @@ changeColor.addEventListener("click", () => {
   } else {
     rootEl.style.setProperty("--primary-color", "#1d1d1d");
     rootEl.style.setProperty("--secondary-color", "#f5f5f5");
+    rootEl.style.setProperty("--tech-color", "#ffaa00");
     nightMode = !nightMode;
     changeColor.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16"> <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/> </svg>
     Light`;
@@ -69,7 +71,7 @@ const handleSubmit = (e) => {
       setTimeout(() => {
         console.log("This is running");
         status.innerText = "";
-      }, 10000);
+      }, 3000);
     });
 };
 
